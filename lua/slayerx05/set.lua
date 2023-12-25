00f7644c4626c59c77e13f319925974889bf8b6f
Pablo Bastidas <pablobastidasv@gmail.com>
2023-12-25 20:19:20 +0100

    Fingers crossed!

--- lua/slayerx05/set.lua
+++ lua/slayerx05/set.lua
@@ -1,21 +1,30 @@
-vim.opt.nu = true
-vim.opt.relativenumber = true
+vim.o.nu = true
+vim.o.relativenumber = true
 
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
 	
-vim.opt.smartindent = true
+vim.o.smartindent = true
 
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
+vim.o.hlsearch = false
+vim.o.incsearch = true
 
-vim.opt.termguicolors = true
+vim.o.termguicolors = true
 
-vim.opt.scrolloff = 8
+vim.o.scrolloff = 8
 
-vim.opt.updatetime = 50
+vim.o.updatetime = 50
 
-vim.opt.colorcolumn = "120"
+vim.o.colorcolumn = "120"
 
+-- save undo history
+vim.o.undofile = true
+
+-- case-insensitive searchin UNLESS \C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Set completeopt to have a better completion experience
+-- vim.o clompleteopt= 'menuone,noselect'
