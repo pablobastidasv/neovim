0bff8e35acefdcac63875f46a23348b279107b2d
Pablo Bastidas <p.bastidas@quin.md>
2023-12-28 12:20:51 +0100

    Changin lualine theme

--- lua/slayerx05/plugins/lualine.lua
+++ lua/slayerx05/plugins/lualine.lua
@@ -4,7 +4,7 @@ return {
   opts = {
     options = {
       icons_enabled = true,
-      theme = 'onedark',
+      theme = 'dracula',
       component_separators = '|',
       section_separators = '',
     },
