be99d6732c1618cfff23644af9662c41614efccb
Pablo Bastidas <p.bastidas@quin.md>
2023-12-28 12:54:10 +0100

    Installing LSP config and Mason

--- lua/slayerx05/plugins/telescope.lua
+++ lua/slayerx05/plugins/telescope.lua
@@ -1,11 +1,28 @@
 return {
-	'nvim-telescope/telescope.nvim', tag = '0.1.5',
-	dependencies = { 'nvim-lua/plenary.nvim' },
-	config = function()
-		local builtin = require('telescope.builtin')
-		vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-		vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-		vim.keymap.set('n', '<leader>fd', builtin.buffers, {})
-		vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-	end,
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.5',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+      vim.keymap.set('n', '<leader>fd', builtin.buffers, {})
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+    end,
+  },
+  {
+    'nvim-telescope/telescope-ui-select.nvim',
+    config = function()
+      local telescope = require('telescope')
+      telescope.setup({
+        extensions = {
+          ['ui-select'] = {
+            require('telescope.themes').get_dropdown {
+            }
+          },
+        },
+      })
+      telescope.load_extension('ui-select')
+    end
+  },
 }
