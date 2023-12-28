958a5b257c758c545ab44f1ed8d71d929901414f
Pablo Bastidas <p.bastidas@quin.md>
2023-12-28 12:18:00 +0100

    Adding neo-tree and renaming colorscheme to refer the plugin

--- lua/slayerx05/plugins/catppuccin.lua
+++ lua/slayerx05/plugins/catppuccin.lua
@@ -2,7 +2,8 @@ return {
 	"catppuccin/nvim", 
 	name = "catppuccin", 
 	priority = 1000,
+  lazy = false,
 	config = function()
-		vim.cmd([[colorscheme catppuccin-macchiato]])
+		vim.cmd.colorscheme "catppuccin-macchiato"
 	end,
 }
