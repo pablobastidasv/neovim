a7959009a694f1a3b07a9c722c697188383c31a3
Pablo Bastidas <pablobastidasv@gmail.com>
2024-01-27 08:45:17 +0100

    Adding LazyGit to interact with GIT from NVIM

---
+++ lua/slayerx05/plugins/git.lua
@@ -0,0 +1,10 @@
+return {
+    -- { "tpope/vim-fugitive" },
+    {
+        "kdheepak/lazygit.nvim",
+        -- optional for floating window border decoration
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+    },
+}
