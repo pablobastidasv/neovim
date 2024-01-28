bc43eb2d4e21b889191bb937f5bdc6b56810ad8a
Pablo Bastidas <pablobastidasv@gmail.com>
2024-01-28 17:51:04 +0100

    Adding golang debugger capabilities

--- lua/slayerx05/plugins/debugging.lua
+++ lua/slayerx05/plugins/debugging.lua
@@ -1,13 +1,15 @@
 return {
 	"mfussenegger/nvim-dap",
 	dependencies = {
+		"leoluz/nvim-dap-go",
 		"rcarriga/nvim-dap-ui",
+		"tpope/vim-fugitive",
 	},
 	config = function()
-		local dap = require("dap")
-		local dapui = require("dapui")
+		require("dapui").setup()
+		require("dap-go").setup()
 
-		dapui.setup()
+		local dap, dapui = require("dap"), require("dapui")
 
 		dap.listeners.before.attach.dapui_config = function()
 			dapui.open()
@@ -15,16 +17,20 @@ return {
 		dap.listeners.before.launch.dapui_config = function()
 			dapui.open()
 		end
-		dap.listeners.before.event_terminated.dapui_config = function()
-			dapui.close()
-		end
-		dap.listeners.before.event_exited.dapui_config = function()
-			dapui.close()
-		end
+		-- dap.listeners.before.event_terminated.dapui_config = function()
+		--     dapui.close()
+		-- end
+		-- dap.listeners.before.event_exited.dapui_config = function()
+		--     dapui.close()
+		-- end
 
+		vim.keymap.set("n", "<Leader>dR", dap.continue, {})
 		vim.keymap.set("n", "<Leader>dr", dap.run_last, {})
 		vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, {})
-		vim.keymap.set("n", "N", dap.continue, {})
+		vim.keymap.set("n", "<Leader>dx", dapui.close, {})
+		vim.keymap.set("n", "M", dap.continue, {})
+		vim.keymap.set("n", "N", dap.step_over, {})
 		vim.keymap.set("n", "E", dap.step_into, {})
+		vim.keymap.set("n", "'", dap.terminate, {})
 	end,
 }
