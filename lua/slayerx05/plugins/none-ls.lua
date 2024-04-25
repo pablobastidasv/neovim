821a1a2c922efd985ec105feb1363062466bf631
Pablo Bastidas <p.bastidas@quin.md>
2024-04-25 21:50:53 +0200

    formatting

--- lua/slayerx05/plugins/none-ls.lua
+++ lua/slayerx05/plugins/none-ls.lua
@@ -1,59 +1,59 @@
 return {
-	{
-		"nvimtools/none-ls.nvim",
-		config = function()
-			local null_ls = require("null-ls")
-			local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+    {
+        "nvimtools/none-ls.nvim",
+        config = function()
+            local null_ls = require("null-ls")
+            local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-			null_ls.setup({
-				sources = {
-					null_ls.builtins.formatting.stylua,
-					null_ls.builtins.formatting.jq,
-					null_ls.builtins.formatting.gofumpt,
-					null_ls.builtins.formatting.goimports_reviser,
-					null_ls.builtins.formatting.golines,
-					null_ls.builtins.formatting.joker,
+            null_ls.setup({
+                sources = {
+                    null_ls.builtins.formatting.stylua,
+                    null_ls.builtins.formatting.jq,
+                    null_ls.builtins.formatting.gofumpt,
+                    null_ls.builtins.formatting.goimports_reviser,
+                    null_ls.builtins.formatting.golines,
+                    null_ls.builtins.formatting.joker,
 
-					null_ls.builtins.diagnostics.vacuum,
-				},
-				on_attach = function(client, bufnr)
-					if client.supports_method("textDocument/formatting") then
-						vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-						vim.api.nvim_create_autocmd("BufWritePre", {
-							group = augroup,
-							buffer = bufnr,
-							callback = function()
-								-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-								-- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
-								vim.lsp.buf.format({ async = false })
-								-- vim.lsp.buf.formatting_sync()
-							end,
-						})
-					end
-				end,
-			})
+                    null_ls.builtins.diagnostics.vacuum,
+                },
+                on_attach = function(client, bufnr)
+                    if client.supports_method("textDocument/formatting") then
+                        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+                        vim.api.nvim_create_autocmd("BufWritePre", {
+                            group = augroup,
+                            buffer = bufnr,
+                            callback = function()
+                                -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+                                -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+                                vim.lsp.buf.format({ async = false })
+                                -- vim.lsp.buf.formatting_sync()
+                            end,
+                        })
+                    end
+                end,
+            })
 
-			vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, {})
-		end,
-	},
-	{
-		"jay-babu/mason-null-ls.nvim",
-		event = { "BufReadPre", "BufNewFile" },
-		dependencies = {
-			"williamboman/mason.nvim",
-			"nvimtools/none-ls.nvim",
-		},
-		config = function()
-			require("mason-null-ls").setup({
-				ensure_installed = {
-					"stylua",
-					"jq",
-					"gofumpt",
-					"goimports-reviser",
-					"golines",
-					"vacuum", -- OpenAPI files
-				},
-			})
-		end,
-	},
+            vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, {})
+        end,
+    },
+    {
+        "jay-babu/mason-null-ls.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = {
+            "williamboman/mason.nvim",
+            "nvimtools/none-ls.nvim",
+        },
+        config = function()
+            require("mason-null-ls").setup({
+                ensure_installed = {
+                    "stylua",
+                    "jq",
+                    "gofumpt",
+                    "goimports-reviser",
+                    "golines",
+                    "vacuum", -- OpenAPI files
+                },
+            })
+        end,
+    },
 }
