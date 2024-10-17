960d2a2086aac037bf4f5f54d1afba1c7144cd93
Pablo Bastidas <pablobastidasv@gmail.com>
2024-10-17 08:57:04 +0200

    updating trouble

--- lua/slayerx05/plugins/trouble.lua
+++ lua/slayerx05/plugins/trouble.lua
@@ -1,16 +1,40 @@
-local prefix = "<leader>x"
-
 return {
     "folke/trouble.nvim",
     dependencies = {
         "nvim-tree/nvim-web-devicons",
     },
-    cmd = { "TroubleToggle", "Trouble" },
+    cmd = { "Trouble" },
     keys = {
-        { prefix,        desc = "Trouble" },
-        { prefix .. "X", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Workspace Diagnostic" },
-        { prefix .. "x", "<cmd>TroubleToggle document_diagnostics<cr>",  desc = "Document Diagnostic" },
-        { prefix .. "q", "<cmd>TroubleToggle quickfix<cr>",              desc = "Quickfix List" },
+        {
+            "<leader>xx",
+            "<cmd>Trouble diagnostics toggle<cr>",
+            desc = "Diagnostics (Trouble)",
+        },
+        {
+            "<leader>xX",
+            "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+            desc = "Buffer Diagnostics (Trouble)",
+        },
+        {
+            "<leader>cs",
+            "<cmd>Trouble symbols toggle focus=false<cr>",
+            desc = "Symbols (Trouble)",
+        },
+        {
+            "<leader>cl",
+            "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+            desc = "LSP Definitions / references / ... (Trouble)",
+        },
+        {
+            "<leader>xL",
+            "<cmd>Trouble loclist toggle<cr>",
+            desc = "Location List (Trouble)",
+        },
+        {
+            "<leader>xQ",
+            "<cmd>Trouble qflist toggle<cr>",
+            desc = "Quickfix List (Trouble)",
+        },
     },
     opts = {},
 }
